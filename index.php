420119f0761fcddc883ef9c6a1822faa99ee1eee
Jaccuse <guck@jack-arnold.com>
2017-10-12 23:25:47 -0600

    Completely forgot to add <head> and <body> tags like an idiot, give me a break I'm tired

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html>
-    <p>
-        <h2>Persona</h2>
-        James has average problems. James wants to complain about them to random people.
-    </p>
-    <p>
-        <h2>Use Case</h2>
-        As a registered user, James wants to create a post.
-    </p>
-    <p>
-        <h2>Interaction Flow</h2>
-        <ol>
-            <li>I type a post and click send</li>
-            <li>The site receives the post and displays it</li>
-        </ol>
-    </p>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Data Design Project</title>
+    </head>
+    <body>
+        <p>
+            <h2>Persona</h2>
+            James has average problems. James wants to complain about them to random people.
+        </p>
+        <p>
+            <h2>Use Case</h2>
+            As a registered user, James wants to create a post.
+        </p>
+        <p>
+            <h2>Interaction Flow</h2>
+            <ol>
+                <li>I type a post and click send</li>
+                <li>The site receives the post and displays it</li>
+            </ol>
+        </p>
+    </body>
 </html>
