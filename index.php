0ff3a1c252e381fd07dcdbbbc25d71daec841ce3
Jaccuse <guck@jack-arnold.com>
2017-10-12 23:23:47 -0600

    Added text to the paragraphs

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
     <p>
-
+        <h2>Persona</h2>
+        James has average problems. James wants to complain about them to random people.
     </p>
     <p>
-
+        <h2>Use Case</h2>
+        As a registered user, James wants to create a post.
+    </p>
+    <p>
+        <h2>Interaction Flow</h2>
+        <ol>
+            <li>I type a post and click send</li>
+            <li>The site receives the post and displays it</li>
+        </ol>
     </p>
 </html>
