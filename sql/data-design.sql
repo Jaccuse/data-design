3ee86a7d7631371996e7fead0da44c7aaee4892e
Jaccuse <guck@jack-arnold.com>
2017-10-17 11:29:12 -0600

    Threw in some more statements ¯\_(ツ)_/¯

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -6,24 +6,41 @@ CREATE TABLE profile (
   profileSalt CHAR(64) NOT NULL,
   UNIQUE(profileEmail),
   UNIQUE(profileName),
-  PRIMARY KEY(profileID)
+  PRIMARY KEY(profileID),
 );
 
 CREATE TABLE post (
   postID BINARY(16) NOT NULL,
   INDEX(postProfileID),
   postProfileID BINARY(16) NOT NULL,
-  postContent VARCHAR(140) NOT NULL, -- not sure what value to assign here
+  postContent VARCHAR(140) NOT NULL,
   postDate DATETIME(6),
   FOREIGN KEY(postProfileID),
-  FOREIGN KEY(postID)
 );
 
 CREATE TABLE comment (
   commentID BINARY(16),
   commentProfileID BINARY(16),
-  commentContent VARCHAR(140), -- again not sure what to put in here
+  commentContent VARCHAR(140),
   commentDate DATETIME(6),
   INDEX(commentProfileID),
-  INDEX(commentID)
+  INDEX(commentID),
+
 );
+
+INSERT INTO profile (profileName, profileEmail) VALUES (1, 2);
+INSERT INTO post (postContent) VALUES (5); -- what am i doing
+INSERT INTO comment (commentContent) VALUES (7);
+
+UPDATE profile
+SET profileID = 2,
+WHERE profileID = 1;
+
+UPDATE post
+SET postProfileID = 2,
+WHERE postProfileID = 1;
+
+UPDATE comment
+SET commentID = 3,
+WHERE commentID = 5;
+
