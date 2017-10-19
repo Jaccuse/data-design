056cca1548673584b70e5c5561bb9fc40d9d6665
Jaccuse <guck@jack-arnold.com>
2017-10-19 11:20:21 -0600

    Added missing foreign keys and indexes

--- sql/reddit.sql
+++ sql/reddit.sql
@@ -1,30 +1,38 @@
 CREATE TABLE profile (
-  profileID BINARY (16) NOT NULL,
+  profileId BINARY (16) NOT NULL,
   profileEmail VARCHAR(128) NOT NULL,
   profileName VARCHAR(32) NOT NULL,
   profileHash CHAR(128) NOT NULL,
   profileSalt CHAR(64) NOT NULL,
   UNIQUE(profileEmail),
   UNIQUE(profileName),
-  PRIMARY KEY(profileID)
+  PRIMARY KEY(profileId)
 );
 
 CREATE TABLE post (
-  postID BINARY(16) NOT NULL,
-  postProfileID BINARY(16) NOT NULL,
+  postId BINARY(16) NOT NULL,
+  postProfileId BINARY(16) NOT NULL,
   postContent VARCHAR(140) NOT NULL,
   postDate DATETIME(6) NOT NULL,
-  INDEX (postContent),
-  FOREIGN KEY(postProfileID) REFERENCES profile(profileID)
+  INDEX(postProfileId),
+  FOREIGN KEY(postProfileId) REFERENCES profile(profileId),
+  PRIMARY KEY(postId)
 );
 
 CREATE TABLE comment (
-  commentID BINARY(16) NOT NULL,
-  commentProfileID BINARY(16) NOT NULL,
+  commentId BINARY(16) NOT NULL,
+  commentProfileId BINARY(16) NOT NULL,
+  commentPostId BINARY(16) NOT NULL,
   commentContent VARCHAR(140) NOT NULL,
   commentDate DATETIME(6) NOT NULL,
-  INDEX(commentProfileID),
-  INDEX(commentID)
+  commentCommentId BINARY(16) NOT NULL,
+  INDEX(commentProfileId),
+  INDEX(commentPostId),
+  INDEX(commentCommentId),
+  FOREIGN KEY(commentCommentId) REFERENCES comment(commentId),
+  FOREIGN KEY(commentPostId) REFERENCES post(postId),
+  FOREIGN KEY(commentProfileId) REFERENCES profile(profileId),
+  PRIMARY KEY(commentId)
 );
 
 -- I'm not sure what values to assign these
