29c660e4cca3719c0082686ccab260fe9208714a
Jaccuse <guck@jack-arnold.com>
2017-10-26 16:38:26 -0600

    Added function for inserting a post into mySQL

--- php/classes/Post.php
+++ php/classes/Post.php
@@ -150,3 +150,20 @@ public function setpostDate($newpostDate) : void {
     }
     $this->postDate = $uuid;
 }
+
+/**
+ * Insert a post into mySQL
+ *
+ * @param \PDO $pdo object for connecting PDO
+ * @throws \PDOException exception thrown when mySQL errors occur
+ * @throws \TypeError when $pdo is not a PDO connector
+ **/
+public function insert(\PDO $pdo) : void {
+    $query = "INSERT INTO post(postId, postDate, postContent, postProfileId) VALUES(:postId, :postDate, :postContent, :postProfileId)";
+    $statement = $pdo->prepare($query);
+
+    $formattedDate = $this->postDateTime->format("Y-m-d H:i:s.u");
+    $parameters = ["postId" => $this->postId->getBytes(), "postUserId" => $this->postUserId->getBytes(), "postTitle" /> $this->postTitle, "postContent" =>$this->postContent, "postDateTime" => $formattedDate];
+    $statement->execute($parameters);
+}
+
